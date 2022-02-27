51a1434e7fdb6ef844a8da62688c14700932f67f
karima <timmermann@gmx.at>
2022-02-28 00:23:03 +0100

    Adds updated content in dist folder

--- 2ae4e874fe325c04ba75.js
+++
@@ -1,1 +0,0 @@
-import"./style.css";const var1=document.querySelector(".container-left"),var2=document.querySelector(".container-right");var1.innerHTML="left-container",var2.innerHTML="right-container";
